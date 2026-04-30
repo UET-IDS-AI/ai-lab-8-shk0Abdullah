dcf32a7e0873ebadf101de99b0f954288ced117c
Abdullah Amjad <shkabdullah268@gmail.com>
2026-04-29 21:58:50 -0700

    ...

--- AI_stats_lab.py
+++ AI_stats_lab.py
@@ -15,7 +15,7 @@ def sigmoid(z):
     """
     sigmoid(z) = 1 / (1 + exp(-z))
     """
-    pass
+    return 1 / (1 + np.exp(-z))
 
 
 def forward_pass(X, W1, W2, W3):
@@ -34,7 +34,10 @@ def forward_pass(X, W1, W2, W3):
     Returns:
         h1, h2, y
     """
-    pass
+    h1 = sigmoid(X.dot(W1))
+    h2 = sigmoid(h1.dot(W2))
+    y = sigmoid(h2.dot(W3))
+    return h1, h2, y
 
 
 def backward_pass(X, h1, h2, y, label, W1, W2, W3):
@@ -44,4 +47,25 @@ def backward_pass(X, h1, h2, y, label, W1, W2, W3):
     Returns:
         dW1, dW2, dW3, loss
     """
-    pass
+    if label == 1:
+        loss = -np.log(y)
+        dJ_dy = -1 / y
+    else:
+        loss = -np.log(1 - y)
+        dJ_dy = 1 / (1 - y)
+
+    dy_dz3 = y * (1 - y)
+    grad3 = dJ_dy * dy_dz3
+    dW3 = h2.T.dot(grad3)
+
+    dJ_dh2 = grad3.dot(W3.T)
+    dh2_dz2 = h2 * (1 - h2)
+    grad2 = dJ_dh2 * dh2_dz2
+    dW2 = h1.T.dot(grad2)
+
+    dJ_dh1 = grad2.dot(W2.T)
+    dh1_dz1 = h1 * (1 - h1)
+    grad1 = dJ_dh1 * dh1_dz1
+    dW1 = X.T.dot(grad1)
+
+    return dW1, dW2, dW3, loss
